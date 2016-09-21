0857d0a5ae069fdf44b49f5846dea71e1c24a76c
Chris Lorton <chrislorton@outlook.com>
2016-09-21 16:42:03 +0100

    Completed functionality - pre jQuery plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
-
-var autoprefixerOptions = {
-  browsers: ['last 6 versions']
-};
+var minify = require('gulp-minifier');
 
 gulp.task('default', ['watch']);
 
 gulp.task('less', function () {
  return gulp.src('./src/less/style.less')
   .pipe(less())
-  .pipe(autoprefixer(autoprefixerOptions))
   .pipe(cleanCSS({compatibility: 'ie8'}))
   .pipe(gulp.dest('./dist/css'))
 });
 
 gulp.task('watch', function () {
     gulp.watch('./src/less/**/*.less', ['less']);
+    gulp.watch('./src/js/**/*.js', ['compress']);
+});
+
+gulp.task('compress', function() {
+    return gulp.src('src/js/*.js').pipe(minify({
+        minify: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+    })).pipe(gulp.dest('dist/js'));
 });
